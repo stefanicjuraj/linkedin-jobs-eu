0ab6284f16c7e7be89771a0b8decd721e8b2cae3
stefanicjuraj <jurajstefanic@outlook.com>
2023-12-10 01:30:14 +0100

    feat: add html to department pages

--- api/departments/data/data.php
+++ api/departments/data/data.php
@@ -1,2 +1,32 @@
 <?php
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>linkedin-jobs</title>
+    <!-- css -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+</head>
+
+<style>
+    body {
+        font-family: 'Inter', sans-serif;
+    }
+</style>
+
+<body>
+
+    <!-- script -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.0.0/flowbite.min.js"></script>
+
+</body>
+
+</html>
